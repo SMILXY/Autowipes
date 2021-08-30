0140e25dda7d7afb4ba2fcd9f2e21c841b42399e
SMILXY <89766729+SMILXY@users.noreply.github.com>
2021-08-29 23:22:40 -0700

    Delete cwipe directory

--- cwipe/cl_cwipe.lua
+++
@@ -1,9 +0,0 @@
-----------CarWipe Every 45 Minutes
-Citizen.CreateThread(function()
-      local x = 2700000 -- 45 Minutes
-      while true do
-       print('CWipe Incoming')
-       Citizen.Wait(x)     
-       ExecuteCommand("carwipe")
-      end
-end)
